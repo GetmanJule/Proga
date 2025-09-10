b90790eaa423be20b58df93fa66059b674f1b1ae
dmitrijmartynov <dotaxai@gmail.com>
2025-09-10 13:26:09 +0300

    fix reorder command

--- Server/src/main/java/org/inner/commands/ReorderCommand.java
+++ Server/src/main/java/org/inner/commands/ReorderCommand.java
@@ -11,13 +11,10 @@ import java.util.Collections;
 public class ReorderCommand implements Command {
     @Override
     public String doo(ArrayList<Movie> mySet, String s) {
-        // Sorts `mySet` itself by movie name, ascending (A→Z)
-        //mySet.sort(Comparator.comparing(Movie::getName));
         Collections.reverse(mySet);
+        new SaveCommand().doo();
         InfoCommand info = new InfoCommand();
-        info.doo(mySet, s);
-
-        return "List is reordered:";
+        return "List is reordered:" + info.doo(mySet, s);
     }
 
     @Override
